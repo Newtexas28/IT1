d099a1e5fcc4d27abf38d05f5cd8bdeb52b2189f
Jon Håkon Lia <jh-lia@online.no>
2022-11-28 13:24:17 +0100

    Adding history.back to all documents in Portefolio

--- Oppgåve_3A/js/main.js
+++ Oppgåve_3A/js/main.js
@@ -7,4 +7,8 @@ let sum = tall1 + tall2;
          
 console.log("Summen blir: " + sum);
 
+console.error("Dette er error");
+console.warn("Dette er warn");
+console.info("Dette er info");
+
 document.getElementById("console").innerHTML = "Summen blir: " + sum;
