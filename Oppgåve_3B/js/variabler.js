2925c4e3ba37048d2faed2ce5901196dbb23104e
Jon Håkon Lia <jh-lia@online.no>
2022-12-01 10:06:09 +0100

    init new files

---
+++ Oppgåve_3B/js/variabler.js
@@ -0,0 +1,14 @@
+
+let favoritt_tall = 28;
+
+console.log(`Mitt favoritt tal er:` +  favoritt_tall)
+
+document.getElementById('favoritt_tall').innerHTML = "Mitt favoritt tall er:" + favoritt_tall;
+
+favoritt_tall = 38
+
+document.getElementById('favoritt_tall_to').innerHTML = "Mitt favoritt tall er:" + favoritt_tall;
+
+let tall = 142;
+
+console.log(tall);  
