5c4234a25de8945bafc1245085681d511879117a
Jon Håkon Lia <jh-lia@online.no>
2022-12-07 09:02:09 +0100

    init JS files

---
+++ Oppgåve_3B/js/oppgåve_4.js
@@ -0,0 +1,7 @@
+let antallTimer = 4;
+let antallMinutter = 27;
+let antallSekunder = 33;
+
+const Total_time = console.log(antallTimer * 3600 + antallMinutter * 60 + antallSekunder);
+
+document.getElementById('Oppgåve_4').innerHTML = Total_time;
