9c54b63267228190226f5c84002b3316c2f9ab7a
Jon Håkon Lia <jh-lia@online.no>
2022-12-10 13:32:26 +0100

    Changing text and error in js file

--- Oppgåve_3A/js/variabler.js
+++ Oppgåve_3A/js/variabler.js
@@ -3,12 +3,12 @@ let favoritt_tall = 28;
 
 console.log(`Mitt favoritt tal er:` +  favoritt_tall)
 
-document.getElementById('favoritt_tall').innerHTML = "Mitt favoritt tall er:" + favoritt_tall;
+document.getElementById('favoritt_tall').innerHTML = "Mitt favoritt tall er: " + favoritt_tall;
 
 favoritt_tall = 38
 
-document.getElementById('favoritt_tall_to').innerHTML = "Mitt favoritt tall er:" + favoritt_tall;
+document.getElementById('favoritt_tall_to').innerHTML = "Mitt favoritt tall er: " + favoritt_tall;
 
 let tall = 142;
 
-console.log(tal);    
+console.log(tall);    
