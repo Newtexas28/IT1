beca33a4d80214d2cba02e8175d24afc9eed2989
Jon Håkon Lia <jh-lia@online.no>
2022-12-19 15:47:11 +0100

    cheeking if boht teams have warriors

---
+++ Let_the_War_begin/js/War_game.js
@@ -0,0 +1,17 @@
+/*
+ * Main file for War Game.
+ * 
+ * 
+ * Author: Jon Håkon Lia <jonlia28@innlandetfylke.no>
+ */
+
+const Team_blue = 100;
+const Team_Red = 56;
+
+if (Team_blue !== Number) {
+    console.log("Blue team dosen have any warriors")
+}
+
+if (Team_Red !== Number) {
+    console.log("Red team dosen`t have any warriors")
+}
